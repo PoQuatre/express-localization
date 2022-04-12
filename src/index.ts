b86051529651c37ab9677b9b2946dfe77e76a888
PoQuatre <leflem.mateo2002@gmail.com>
2022-04-12 15:22:28 +0200

    feat: add translations

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import 'dotenv/config';
 import express from 'express';
 import { engine } from 'express-handlebars';
 
+import translations from './translations.json';
+
 const port = parseInt(process.env.PORT || '') || 8080;
 const app = express();
 
@@ -13,8 +15,22 @@ app.set('view engine', 'hbs');
 // Setup the public folder
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/:lang?', (req, res) => {
+  const rawLang = req.params.lang?.toLowerCase() || '';
+  const lang = (
+    Object.keys(translations as {}).includes(rawLang) ? rawLang : 'fr'
+  ) as keyof typeof translations;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  const translation = translations[lang] as {
+    pageTitle: string;
+    title: string;
+  };
+
+  res.render('index', {
+    pageTitle: translation.pageTitle,
+    title: translation.title,
+  });
 });
 
 // Starts listening on port 8080 or the port specified in .env
